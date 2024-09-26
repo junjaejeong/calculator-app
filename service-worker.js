d8258e6b4b624470174871bd862549c3ad63b97f
junjaejeong <125377749+junjaejeong@users.noreply.github.com>
2024-09-26 20:33:12 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v3').then((cache) => {
+    caches.open('v4').then((cache) => {
       return cache.addAll([
         './index.html',
         './style.css',
