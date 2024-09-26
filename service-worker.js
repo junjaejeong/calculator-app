d44bb6ecd615eed2e85dc78f68e78b566b249d4f
junjaejeong <125377749+junjaejeong@users.noreply.github.com>
2024-09-26 20:13:11 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v3').then((cache) => {
+      return cache.addAll([
+        './index.html',
+        './style.css',
+        './script.js',
+        './계산기.PNG'  // 아이콘 파일 추가
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
