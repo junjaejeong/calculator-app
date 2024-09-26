ca9da8341261cf6ba6a4324526810293347b172d
junjaejeong <125377749+junjaejeong@users.noreply.github.com>
2024-09-27 07:35:03 +0900

    Update sevice-worker.js

--- sevice-worker.js
+++ sevice-worker.js
@@ -1,9 +1,10 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v4').then((cache) => {  // 캐시 버전 v4로 변경
+    caches.open('v5').then((cache) => {  // 캐시 버전 v4로 변경
       return cache.addAll([
         '/',
         '/index.html',
+        '/style.css',
         '/script.js',
         '/aaa.png'
       ]);
